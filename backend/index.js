ec74d21b431c9bd800341ff21acdc3c3bbd67c0f
ismailalakhras <ismail.malakhras@gmail.com>
2024-07-21 18:51:42 +0300

    create new role

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,24 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 require("./models/db");
-const app = express();
-const PORT = process.env.PORT;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
+
+//routers
+const rolesRouter = require("./routes/role");
+
+
+
+// router middleware
+app.use("/roles", rolesRouter);
+
+
+
+const PORT = process.env.PORT || 5001;
+
+
 
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
