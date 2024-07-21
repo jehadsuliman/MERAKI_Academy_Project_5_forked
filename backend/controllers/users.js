470723b7258be9cfa4a8cffb729bb9b6335e94cc
ismailalakhras <ismail.malakhras@gmail.com>
2024-07-21 20:39:50 +0300

    userLogin

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const { pool } = require("../models/db");
+const jwt = require("jsonwebtoken");
 const saltRounds = parseInt(process.env.SALT);
 
 
@@ -65,8 +66,57 @@ const userRegister = async (req, res) => {
         })
 };
 
+const userLogin = (req, res) => {
+    const { email, password } = req.body
+    const query = `SELECT * FROM users WHERE email = $1`;
+    const data = [email.toLowerCase()];
+    pool
+        .query(query, data)
+        .then((result) => {
+            if (result.rows.length) {
+                bcrypt.compare(password, result.rows[0].password, (err, response) => {
+                    if (err) res.json(err);
+                    if (response) {
+                        const payload = {
+                            userId: result.rows[0].id,
+                            country: result.rows[0].country,
+                            role: result.rows[0].role_id,
+                        };
+                        const options = { expiresIn: "1d" };
+                        const secret = process.env.SECRET;
+                        const token = jwt.sign(payload, secret, options);
+                        if (token) {
+                            return res.status(200).json({
+                                token,
+                                success: true,
+                                message: `Valid login credentials`,
+                                userId: result.rows[0].id
+                            });
+                        } else {
+                            throw Error;
+                        }
+                    } else {
+                        res.status(403).json({
+                            success: false,
+                            message: `The email doesn’t exist or the password you’ve entered is incorrect`,
+                        });
+                    }
+                });
+            } else throw Error;
+        })
+        .catch((err) => {
+            res.status(403).json({
+                success: false,
+                message:
+                    "The email doesn’t exist or the password you’ve entered is incorrect",
+                err,
+            });
+        });
+};
+
 
 
 module.exports = {
-    userRegister
+    userRegister,
+    userLogin
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { userRegister } = require("../controllers/users");
+const { userRegister, userLogin } = require("../controllers/users");
 
 
 
 const usersRouter = express.Router();
 
 usersRouter.post("/register", userRegister);
+usersRouter.post("/login", userLogin);
 
 
 
