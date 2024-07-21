fba2534a7e1e3957cfb256ad43bc75be5ecbe26e
ismailalakhras <ismail.malakhras@gmail.com>
2024-07-21 19:14:34 +0300

    create permission

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -23,7 +23,33 @@ const createNewRole = (req, res) => {
     });
 };
 
+
+const createNewPermission = (req, res) => {
+  const { permission } = req.body;
+  const query = `INSERT INTO permissions (permission) VALUES ($1) RETURNING *;`;
+  const data = [permission];
+
+  pool
+    .query(query, data)
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `Permission created successfully`,
+        result: result.rows,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server error`,
+        err: err,
+      });
+    });
+};
+
+
 module.exports = {
-    createNewRole
+    createNewRole,
+    createNewPermission
   };
   
