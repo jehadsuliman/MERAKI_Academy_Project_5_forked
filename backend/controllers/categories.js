82740f0878dd72689beab258ae641c157d0df3f0
KhaledOdehh <[200~khaled.awd.odeh@gmail.com>
2024-07-21 22:15:12 +0300

    meaningful commit

---
+++ backend/controllers/categories.js
@@ -0,0 +1,27 @@
+const { pool } = require("../models/db");
+const createNewCategory = (req, res) => {
+  const { name, image } = req.body;
+  const user_id = req.token.userId;
+  pool
+    .query(
+      `INSERT INTO categories (name,image,user_id)VALUES ($1, $2,$3) RETURNING *;`,
+      [name, image, user_id]
+    )
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        massage: "Category created successfully",
+        Category: result.rows[0],
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        massage: "Server error",
+        error: err,
+      });
+    });
+};
+module.exports = {
+  createNewCategory,
+};
---
+++ backend/routes/categories.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { createNewCategory } = require("../controllers/categories");
+const authentication = require("../middleware/authentication");
+const categoriesRouter = express.Router();
+
+categoriesRouter.post("/", authentication, createNewCategory);
+
+module.exports = categoriesRouter;
