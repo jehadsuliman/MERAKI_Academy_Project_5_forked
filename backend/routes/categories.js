363c1d835777d4cbf1cd35eb5e66b4d19d7bf775
KhaledOdehh <[200~khaled.awd.odeh@gmail.com>
2024-07-22 00:41:14 +0300

    update Category By Id

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -4,6 +4,7 @@ const {
   createNewCategory,
   getAllCategories,
   getCategoryById,
+  updateCategoryById
 } = require("../controllers/categories");
 const authentication = require("../middleware/authentication");
 const categoriesRouter = express.Router();
@@ -11,5 +12,7 @@ const categoriesRouter = express.Router();
 categoriesRouter.post("/", authentication, createNewCategory);
 categoriesRouter.get("/", getAllCategories);
 categoriesRouter.get("/:id", getCategoryById);
+categoriesRouter.put("/:id", updateCategoryById);
+
 
 module.exports = categoriesRouter;
