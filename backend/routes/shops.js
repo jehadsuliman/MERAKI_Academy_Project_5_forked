b0e34dcdfd59cce56810a45b5f8b961e1ca6b29a
ismailalakhras <ismail.malakhras@gmail.com>
2024-07-22 16:42:12 +0300

    updateShopById

--- backend/routes/shops.js
+++ backend/routes/shops.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { shopRegister, shopLogin, getAllShops, getShopByCategoryId, getShopById } = require("../controllers/shops");
+const { shopRegister, shopLogin, getAllShops, getShopByCategoryId, getShopById, updateShopById } = require("../controllers/shops");
 
 
 
@@ -10,6 +10,8 @@ shopsRouter.post("/login", shopLogin);
 shopsRouter.get("/", getAllShops);
 shopsRouter.get("/:id", getShopById);
 shopsRouter.get("/:id/category", getShopByCategoryId);
+shopsRouter.put("/:id", updateShopById);
+
 
 
 
