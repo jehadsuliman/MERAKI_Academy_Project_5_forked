b71052187ed1368c4c9508005307a3c983e08fe0
jehadsuliman <jehad.msuliman@gmail.com>
2024-07-22 15:40:12 +0300

    update Sub Category By Id

--- backend/controllers/subCategories.js
+++ backend/controllers/subCategories.js
@@ -1,8 +1,7 @@
 const { pool } = require("../models/db");
 
-
 const createNewSubCategory = (req, res) => {
-  const shop_id = req.token.shopId
+  const shop_id = req.token.shopId;
   const { description } = req.body;
   pool
     .query(
@@ -30,7 +29,6 @@ const getAllSubCategories = (req, res) => {
 
     .query(`SELECT * FROM sub_categories WHERE is_deleted = 0;`)
 
-
     .then((result) => {
       res.status(200).json({
         success: true,
@@ -48,21 +46,22 @@ const getAllSubCategories = (req, res) => {
 };
 
 const getSubCategoryById = (req, res) => {
- const subCategoryId = req.params.id;
+  const subCategoryId = req.params.id;
 
   console.log(subCategoryId);
   pool
-    .query(`SELECT * FROM sub_categories WHERE id = $1 AND is_deleted=0;`, [subCategoryId])
+    .query(`SELECT * FROM sub_categories WHERE id = $1 AND is_deleted = 0;`, [
+      subCategoryId,
+    ])
     .then((result) => {
-      console.log(result.rows)
+      console.log(result.rows);
       if (result.rows.length > 0) {
         res.status(200).json({
           success: true,
           message: `The sub category with id: ${subCategoryId}`,
           subCategories: result.rows[0],
         });
-      } 
-      else {
+      } else {
         res.status(404).json({
           success: false,
           message: `The sub category with id: ${subCategoryId} not found`,
@@ -79,29 +78,28 @@ const getSubCategoryById = (req, res) => {
 };
 
 const updateSubCategoryById = (req, res) => {
-  subCategoryId = req.params.id;
-  const { description, shop_id } = req.body;
+  const subCategoryId = req.params.id;
+  const { description } = req.body;
   pool
     .query(
       `UPDATE sub_categories
     SET 
-      description = COALESCE($1, description),
-      shop_id = COALESCE($2, shop_id)
-    WHERE id = $3
+      description = COALESCE($1, description)
+    WHERE id = $2
     RETURNING *;`,
-      [description, shop_id, subCategoryId]
+      [description, subCategoryId]
     )
     .then((result) => {
       if (result.rows.length > 0) {
         res.status(200).json({
           success: true,
-          message: `Sub Category with id: ${id} updated successfully`,
+          message: `Sub Category with id: ${subCategoryId} updated successfully`,
           subCategories: result.rows[0],
         });
       } else {
         res.status(404).json({
           success: false,
-          message: `Sub Category with id: ${id} Not Found`,
+          message: `Sub Category with id: ${subCategoryId} Not Found`,
         });
       }
     })
