14b1a7db159595c68a442ac53b251049a4d2a434
ismailalakhras <ismail.malakhras@gmail.com>
2024-07-24 21:27:36 +0300

    delete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./components/User/HomePage/HomePage";
 
 //===============================================================
 
@@ -8,7 +7,6 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path={"/users/HomePage"} element={<HomePage />} />
       </Routes>
     </div>
   );
