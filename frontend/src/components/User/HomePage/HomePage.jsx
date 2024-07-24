5adbd17a04b7b9b60dfa41a0d83355f57581ffbe
ismailalakhras <ismail.malakhras@gmail.com>
2024-07-24 14:29:05 +0300

    headerComponent

---
+++ frontend/src/components/User/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header_top from "../components/Header/Header_top";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header_top />
+    </div>
+  );
+};
+
+export default HomePage;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,15 +7,15 @@ import App from "./App";
 // import Provider
 import { Provider } from "react-redux";
 // import the store
-import store from "./redux/store";
+// import store from "./redux/store";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-      <Provider store={store}>
+      {/* <Provider store={store}> */}
         <App />
-      </Provider>
+      {/* </Provider> */}
   </Router>
 );
 
