17dd2dfde4b3d3d57e6cfe38fd284b6a524c9e9a
ismailalakhras <ismail.malakhras@gmail.com>
2024-07-24 11:49:02 +0300

    edit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+// import Provider
+import { Provider } from "react-redux";
+// import the store
+import store from "./redux/store";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+  </Router>
+);
+
+
