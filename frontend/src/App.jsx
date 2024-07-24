47ceab8cea7f9d3fed5fba2a0601caa9bc17e00f
KhaledOdehh <[200~khaled.awd.odeh@gmail.com>
2024-07-24 23:27:09 +0300

    redux And Router folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
-
+import {RouterProvider} from "react-router-dom"
+import { router } from "./Routers";
 function App() {
-  return <></>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
