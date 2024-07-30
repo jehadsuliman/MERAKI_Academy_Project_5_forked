d2eb2f3095c232cfcfb75099ca44590edd0e1d8b
jehadsuliman <jehad.msuliman@gmail.com>
2024-07-30 21:59:47 +0300

    Select Category In Register

--- frontend/src/layouts/Shop/Register.jsx
+++ frontend/src/layouts/Shop/Register.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const ShopRegister = () => {
   const navigate = useNavigate();
-  const [from, setFrom] = useState({
+  const [form, setForm] = useState({
     shopName: "",
     country: "",
     phone_number: "",
@@ -14,16 +14,44 @@ const ShopRegister = () => {
     password: "",
     category_id: "",
   });
+  const [categories, setCategories] = useState([]);
+  const [selectedCategoryName, setSelectedCategoryName] = useState("");
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/categories");
+        if (response.data && Array.isArray(response.data.categories)) {
+          setCategories(response.data.categories);
+        } else {
+          console.error("Invalid data format", response.data);
+        }
+      } catch (error) {
+        console.error("Failed to fetch categories", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
 
   const handleChange = (e) => {
-    setFrom({ ...from, [e.target.name]: e.target.value });
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleCategoryChange = (e) => {
+    const selectedCategoryId = e.target.value;
+    setForm({ ...form, category_id: selectedCategoryId });
+    const selectedCategory = categories.find(
+      (category) => category.id === selectedCategoryId
+    );
+    setSelectedCategoryName(selectedCategory.name);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const dataToSend = {
-      ...from,
+      ...form,
       role_id: 3,
     };
     console.log(dataToSend);
@@ -44,53 +72,60 @@ const ShopRegister = () => {
       <input
         type="text"
         name="shopName"
-        value={from.shopName}
+        value={form.shopName}
         onChange={handleChange}
         placeholder="Shop Name"
       />
       <input
         type="text"
         name="country"
-        value={from.country}
+        value={form.country}
         onChange={handleChange}
         placeholder="Country"
       />
       <input
         type="text"
         name="phone_number"
-        value={from.phone_number}
+        value={form.phone_number}
         onChange={handleChange}
         placeholder="Phone Number"
       />
       <input
         type="email"
         name="email"
-        value={from.email}
+        value={form.email}
         onChange={handleChange}
         placeholder="Email"
       />
       <input
-        type="discreption"
+        type="text"
         name="discreption"
-        value={from.discreption}
+        value={form.discreption}
         onChange={handleChange}
         placeholder="Description"
       />
       <input
         type="password"
         name="password"
-        value={from.password}
+        value={form.password}
         onChange={handleChange}
         placeholder="Password"
       />
-      <input
-        type="text"
-        name="category_id"
-        value={from.category_id}
-        onChange={handleChange}
-        placeholder="Category ID"
-      />
-
+      <div>
+        <label>Category:</label>
+        <select
+          name="category_id"
+          value={form.category_id}
+          onChange={handleCategoryChange}
+        >
+          <option value="">Select a category</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
       <button type="submit">Register</button>
     </form>
   );
