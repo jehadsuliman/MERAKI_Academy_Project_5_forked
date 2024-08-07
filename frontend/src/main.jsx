8c73a3d855beab229bbf08bc37de5e7d2fe3863f
jehadsuliman <jehad.msuliman@gmail.com>
2024-08-07 22:43:29 +0300

    Login with google

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from "./Service/api/redux/store.jsx"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import store from "./Service/api/redux/store.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <GoogleOAuthProvider clientId="815765456706-kngddni8k44ucnp5vcbv5k6r2cdkh3v8.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
     </Provider>
   </React.StrictMode>
 );
